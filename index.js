3e702c186b859e12eb84dd8ddab1a879840aa3fc
Brad Vogel <brad@mixmax.com>
2017-09-04 16:42:27 -0700

    Add transpilation so we can use it in Node v6.

---
+++ index.js
@@ -0,0 +1,9 @@
+const semver = require('semver');
+
+let main;
+if (semver.lt(process.version, '7.6.0')) {
+  main = require('./dist/node/index.js');
+} else {
+  main = require('./index.js');
+}
+module.exports = main;
